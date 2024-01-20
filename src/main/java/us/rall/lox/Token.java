fdd4f71c05937d8f7f2cefb664d4c6e06f1cee96
Adam Rall <ralladamr@gmail.com>
2024-01-20 11:06:00 -0500

    Add tokens

---
+++ src/main/java/us/rall/lox/Token.java
@@ -0,0 +1,31 @@
+package us.rall.lox;
+
+/**
+ * Represents a Lox token.
+ */
+public class Token {
+    private final TokenType type;
+    private final String lexeme;
+    private final Object literal;
+    private final int line;
+
+    /**
+     * Class constructor specifying all parts of token.
+     *
+     * @param type    The type of the token.
+     * @param lexeme  The parsed lexeme.
+     * @param literal The value of the token.
+     * @param line    The source line of the token.
+     */
+    public Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}

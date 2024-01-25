a2842f600f23af2fd0e9e76b0d06afca53a90061
Adam Rall <ralladamr@gmail.com>
2024-01-25 09:58:25 -0500

    Add for statements

--- src/main/java/us/rall/lox/Parser.java
+++ src/main/java/us/rall/lox/Parser.java
@@ -1,6 +1,7 @@
 package us.rall.lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -115,6 +116,9 @@ class Parser {
     }
 
     private Stmt statement() {
+        if (match(TokenType.FOR)) {
+            return forStatement();
+        }
         if (match(TokenType.IF)) {
             return ifStatement();
         }
@@ -128,6 +132,43 @@ class Parser {
             return new Stmt.Block(block());
         }
         return expressionStatement();
+    }
+
+    private Stmt forStatement() {
+        consume(TokenType.LEFT_PAREN, "Expect '(' after 'for'.");
+        Stmt initializer;
+        if (match(TokenType.SEMICOLON)) {
+            initializer = null;
+        } else if (match(TokenType.VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+        if (!check(TokenType.SEMICOLON)) {
+            condition = expression();
+        }
+        consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+
+        Expr increment = null;
+        if (!check(TokenType.SEMICOLON)) {
+            increment = expression();
+        }
+        consume(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        Stmt body = statement();
+        if (increment != null) {
+            body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+        }
+        if (condition == null) {
+            condition = new Expr.Literal(true);
+        }
+        body = new Stmt.While(condition, body);
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+        return body;
     }
 
     private Stmt ifStatement() {
